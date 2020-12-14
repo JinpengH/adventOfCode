8611f3463ad2a62fbba708c55d4907c1a42d49cb
Jinpeng He <jinpengh@usc.edu>
2020-12-14 20:42:01 +0800

    day10

--- src/Day.java
+++ src/Day.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 public class Day
 {
@@ -27,6 +26,16 @@ public class Day
             case 6:
                 day6();
                 break;
+            case 7:
+                day7();
+                break;
+            case 8:
+                day8();
+                break;
+            case 9:
+                day9();
+                break;
+
             default:
                 break;
         }
@@ -135,8 +144,8 @@ public class Day
                     else if(c == ' '){max = Integer.parseInt(sb.toString());}
                     else{sb.append(c);}
                 }
-                if(Day2Helper(min,max,reg,password)){total++;}
-                if(Day2EXHelper(min,max,reg,password)){totalEX++;}
+                if(day2Helper(min,max,reg,password)){total++;}
+                if(day2EXHelper(min,max,reg,password)){totalEX++;}
             }
             System.out.println("DAY2: " + total);
             System.out.println("DAY2EX: " + totalEX);
@@ -151,7 +160,7 @@ public class Day
         }
     }
 
-    private boolean Day2Helper(int min, int max, char reg, String password)
+    private boolean day2Helper(int min, int max, char reg, String password)
     {
         int total = 0;
         for(char c : password.toCharArray())
@@ -161,7 +170,7 @@ public class Day
         return total >= min && total <= max;
     }
 
-    private boolean Day2EXHelper(int min, int max, char reg, String password)
+    private boolean day2EXHelper(int min, int max, char reg, String password)
     {
         return password.charAt(min-1) == reg ^ password.charAt(max-1) == reg;
     }
@@ -414,43 +423,329 @@ public class Day
     private void day7()
     {
         File file = new File("day7.txt");
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String str;
+            HashMap<String, Set<String>> outMap = new HashMap<>();
+            HashMap<String, Map<String, Integer>> inMap = new HashMap<>();
+            HashSet<String> set = new HashSet<>();
+
+
+            while((str = reader.readLine()) != null)
+            {
+                bagParser(str, outMap, inMap);
+            }
+
+
+            Queue<String> queue = new LinkedList<>();
+            queue.offer("shiny gold");
+
+
+            while(!queue.isEmpty())
+            {
+
+                String bag = queue.poll();
+                //System.out.println("DEBUG: " + bag);
+                set.add(bag);
+                if(outMap.get(bag) == null){continue;}
+                for(String s : outMap.get(bag))
+                {
+                    if(!set.contains(s)){queue.add(s);set.add(s);}
+                }
+            }
+
+            HashMap<String, Integer> mem = new HashMap<>();
+            String tar = "shiny gold";
+
+            int totalEX = getBags(tar, inMap, mem);
+            for(Map.Entry<String, Integer> entry : mem.entrySet()){
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+            System.out.println("DAY7: " + (set.size()-1));
+            System.out.println("DAY7EX: " + (totalEX - 1));
+        }
+        catch(FileNotFoundException fe)
+        {
+            System.out.println("Cannot find the file");
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("Read error");
+        }
+
+    }
+
+    private void bagParser(String str, HashMap<String, Set<String>> outMap, HashMap<String, Map<String, Integer>> inMap)
+    {
+        String[] strArr = str.split(" ");
+        String container = strArr[0] + " " + strArr[1];
+        if(strArr[4].equals("no")){return;}
+        for(int i = 5; i < strArr.length-1; i+=4)
+        {
+            String contained = strArr[i] + " " + strArr[i+1];
+            if(!outMap.containsKey(contained)){outMap.put(contained, new HashSet<String>());}
+            if(!inMap.containsKey(container)){inMap.put(container, new HashMap<String, Integer>());}
+            outMap.get(contained).add(container);
+            inMap.get(container).put(contained, Integer.parseInt(strArr[i-1]));
+            //System.out.println("DEBUG: " + container + " contains " + strArr[i-1] + " "+ contained);
+            //System.out.println("DEBUG: " + container + " contains " + contained);
+        }
+    }
+
+    private int getBags(String str, HashMap<String, Map<String, Integer>> inMap, Map<String, Integer> mem)
+    {
+        if(!inMap.containsKey(str)){return 1;}
+        int total = 1;
+        for(Map.Entry<String, Integer> entry : inMap.get(str).entrySet())
+        {
+            total += entry.getValue() * getBags(entry.getKey(), inMap, mem);
+        }
+        mem.put(str,total);
+        return total;
+    }
+
+    private void day8()
+    {
+        File file = new File("day8.txt");
 
         try
         {
-            HashSet<Character> set = new HashSet<>();
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String str = "";
-            int total = 0;
-            int totalEX = 0;
-            int line = 0;
-            int[] alp = new int[26];
+            String str;
+            ArrayList<String> list = new ArrayList<>();
             while((str = reader.readLine()) != null)
             {
-                if(str.equals(""))
+                list.add(str);
+            }
+
+
+            Set<Integer> set = new HashSet<>();
+            int i = 0;
+            int acc = 0;
+            while(!set.contains(i))
+            {
+                String instruction = list.get(i).substring(0,3);
+                int op = Integer.parseInt(list.get(i).substring(4));
+                set.add(i);
+                switch (instruction) {
+                    case "nop":
+                        i++;
+                        break;
+                    case "acc":
+                        i++;
+                        acc += op;
+                        break;
+                    case "jmp":
+                        i += op;
+                        break;
+                }
+            }
+            System.out.println("DAY8: " + acc);
+
+
+            i = 0;
+            acc = 0;
+            boolean flag = false;
+            while(i <= list.size() || flag)
+            {
+                String instruction = list.get(i).substring(0,3);
+                int op = Integer.parseInt(list.get(i).substring(4));
+                set.add(i);
+                switch (instruction) {
+                    case "nop":
+                        if(!set.contains(i+op))
+                        {
+                            int res = testRun(i+op,acc,list,set);
+                            if (res != -1)
+                            {
+                                flag = true;
+                                System.out.println("DAY8EX: " + res);
+                            }
+                        }
+                        i++;
+                        break;
+                    case "acc":
+                        i++;
+                        acc += op;
+                        break;
+                    case "jmp":
+                        if(!set.contains(i+1))
+                        {
+                            int res = testRun(i+1,acc,list,set);
+                            if (res != -1)
+                            {
+                                flag = true;
+                                System.out.println("DAY8EX: " + res);
+                            }
+                        }
+                        i += op;
+                        break;
+                }
+            }
+
+        }
+        catch(FileNotFoundException fe)
+        {
+            System.out.println("Cannot find the file");
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("Read error");
+        }
+
+    }
+
+    private int testRun(int i, int acc, List<String> list, Set<Integer> set)
+    {
+        while(i < list.size())
+        {
+            if(set.contains(i)){return -1;}
+            String instruction = list.get(i).substring(0,3);
+            int op = Integer.parseInt(list.get(i).substring(4));
+            set.add(i);
+            switch (instruction) {
+                case "nop":
+                    i++;
+                    break;
+                case "acc":
+                    i++;
+                    acc += op;
+                    break;
+                case "jmp":
+                    i += op;
+                    break;
+            }
+        }
+        return acc;
+    }
+
+    private void day9()
+    {
+        File file = new File("day9.txt");
+        boolean flag = true;
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String str;
+            Set<Long> set = new HashSet<>();
+            Queue<Long> queue = new LinkedList<>();
+            List<Long> list = new ArrayList<>();
+            long par = 0;
+            while((str = reader.readLine()) != null && flag)
+            {
+                par = Long.parseLong(str);
+                list.add(par);
+                if(set.size() < 25)
                 {
-                    total = total + set.size();
-                    set = new HashSet<>();
-                    for(int i : alp)
-                    {
-                        if(i == line){totalEX++;}
-                    }
-                    line = 0;
-                    alp = new int[26];
+                    set.add(par);
+                    queue.offer(par);
                 }
                 else
                 {
-                    for(char c : str.toCharArray())
+                    boolean isPaired = false;
+                    for(int i = 0; i < 25; i++)
                     {
-                        set.add(c);
-                        alp[c-'a']++;
+                        long popped = queue.poll();
+                        //System.out.println("DEBUG: removed " + popped);
+                        queue.offer(popped);
+                        if(set.contains(par-popped) && !isPaired)
+                        {
+                            //System.out.println("DEBUG: Paired " + popped + " " + (par-popped));
+                            isPaired = true;
+                        }
                     }
-                    line++;
+                    if(!isPaired){
+                        System.out.println("DAY9: " + par);
+                        flag = false;
+                    }
+                    long k = queue.poll();
+                    //System.out.println("DEBUG: removed " + k);
+                    set.remove(k);
+                    set.add(par);
+                    queue.offer(par);
+
                 }
             }
-            //total = total + set.size();
-            System.out.println("DAY6: " + total);
-            System.out.println("DAY6EX: " + totalEX);
 
+            //EX
+            List<Long> sum = new ArrayList<>();
+            long s = 0;
+            for(int i = 0; i < list.size()-1; i++)
+            {
+                s = s + list.get(i);
+                sum.add(s);
+            }
+            for(int i = 0; i < sum.size()-1; i++)
+            {
+                for(int j = i+1; j < sum.size(); j++)
+                {
+                    if(sum.get(j) - sum.get(i) == par)
+                    {
+                        System.out.println("DAY9EX: " + (i+1) + " " + j);
+                        break;
+                    }
+                }
+            }
+            long min = list.get(570);
+            long max = list.get(554);
+            for(int i = 554; i <= 570; i++)
+            {
+                min = Math.min(list.get(i),min);
+                max = Math.max(list.get(i),max);
+            }
+            System.out.println(min+max);
+
+        }
+        catch(FileNotFoundException fe)
+        {
+            System.out.println("Cannot find the file");
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("Read error");
+        }
+
+    }
+
+    private void day10()
+    {
+        File file = new File("day6.txt");
+
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String str;
+            while((str = reader.readLine()) != null)
+            {
+
+
+            }
+        }
+        catch(FileNotFoundException fe)
+        {
+            System.out.println("Cannot find the file");
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("Read error");
+        }
+
+    }
+
+    private void dayEX()
+    {
+        File file = new File("day6.txt");
+
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String str;
+            while((str = reader.readLine()) != null)
+            {
+
+
+            }
         }
         catch(FileNotFoundException fe)
         {
